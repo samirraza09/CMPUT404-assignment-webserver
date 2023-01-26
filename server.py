69524d1e8ed4d4f3531c16b777c70e9bf916aa6b
Samir <ssraza@ualberta.ca>
2023-01-26 10:58:27 -0700

    server.py handles all requirements and updated license and readme

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import os.path
+from os import path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,12 +30,59 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    host = "localhost:8080"
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        requestBody = self.data.decode('utf-8')
+        requestLines = requestBody.split('\r\n')
+        firstLine = requestLines[0]
+        firstLineSplit = firstLine.split(' ');
+        requestType = firstLineSplit[0]
+        filePath = firstLineSplit[1]
+        
+        if len(requestLines) == 0 or len(requestType) == 0 or requestType.lower() != 'get':
+            requestToSend = "HTTP/1.1 405 Method Not Allowed\r\nHost: " + self.host + "\n\n"
+            print("Thrown 405")
+            self.request.sendall(bytearray(requestToSend,'utf-8'))
 
+        elif not path.exists('www' + filePath) or '/..' in filePath:
+            requestToSend = "HTTP/1.1 404 Not Found\r\nHost: " + self.host + "\n\n"
+            self.request.sendall(bytearray(requestToSend,'utf-8'))
+
+        elif path.isfile('www' + filePath):
+            if filePath[-4:] == '.css':
+                requestToSend = "HTTP/1.1 200 OK\r\nHost: " + self.host + "\r\nContent-Type: text/css\n\n"
+            
+                # https://flaviocopes.com/python-read-file-content/
+                requestToSend += open('./www' + filePath, 'r').read();
+                self.request.sendall(bytearray(requestToSend,'utf-8'))
+
+            requestToSend = "HTTP/1.1 200 OK\r\nHost: " + self.host + "\r\nContent-Type: text/html\n\n"
+            
+            # https://flaviocopes.com/python-read-file-content/
+            requestToSend += open('./www' + filePath, 'r').read();
+            self.request.sendall(bytearray(requestToSend,'utf-8'))
+
+        # If the last character in the path is a / then add index.html
+        elif filePath[len(filePath) - 1] == '/':
+            filePath += 'index.html'
+            requestToSend = "HTTP/1.1 200 OK\r\nHost: " + self.host + "\r\nContent-Type: text/html\n\n"
+            
+            # https://flaviocopes.com/python-read-file-content/
+            requestToSend += open('./www' + filePath, 'r').read();
+            self.request.sendall(bytearray(requestToSend,'utf-8'))
+
+        # If the path ends with a directory but is missing a / send a 301
+        elif path.exists('www' + filePath) and filePath[len(filePath) - 1] != '/':
+            relocatedLocation = filePath + "/"
+
+            requestToSend = "HTTP/1.1 301 Permanently Moved\r\nHost: " + self.host + "\r\nLocation "
+            requestToSend += relocatedLocation + "\n\n"
+            self.request.sendall(bytearray(requestToSend,'utf-8'))
+ 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
